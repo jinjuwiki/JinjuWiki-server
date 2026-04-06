acb000dad3123fa0ba02c0d0772bf758b73dc04a
김우성 <azunox9@gmail.com>
2026-04-06 11:31:18 +0900

    hotfix : JWT 인증 관련 예외 시 500 -> 401 Error로 변경

--- src/main/java/com/jinju/jinjuwiki/global/security/UserPrincipal.java
+++ src/main/java/com/jinju/jinjuwiki/global/security/UserPrincipal.java
@@ -34,6 +34,7 @@ public class UserPrincipal implements UserDetails {
         return id;
     }
 
+    // 권한 설정
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role));
