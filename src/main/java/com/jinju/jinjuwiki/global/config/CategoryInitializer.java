6bc52f7936365f888949314c81d9160202307aa4
김우성 <azunox9@gmail.com>
2026-04-08 10:23:44 +0900

    refactor : private final를 사용하여 상수로 분리

--- src/main/java/com/jinju/jinjuwiki/global/config/CategoryInitializer.java
+++ src/main/java/com/jinju/jinjuwiki/global/config/CategoryInitializer.java
@@ -12,13 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class CategoryInitializer {
 
+    // 상수화
+    private static final List<String> DEFAULT_CATEGORIES = List.of("학교", "학생", "사건사고", "선생님", "기타");
+
     private final CategoryRepository categoryRepository;
 
     @Bean
     public CommandLineRunner initCategories() {
         return args -> {
-            List<String> defaultCategories = List.of("학교", "학생", "사건사고", "선생님", "기타"); // 카테고리 수정 가능
-            List<Category> categories = defaultCategories.stream()
+            List<Category> categories = DEFAULT_CATEGORIES.stream()
                     .filter(name -> !categoryRepository.existsByName(name))
                     .map(name -> Category.builder().name(name).build())
                     .toList();
