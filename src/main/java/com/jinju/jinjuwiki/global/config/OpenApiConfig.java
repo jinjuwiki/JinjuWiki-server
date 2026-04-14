0b4187047d8aace6d04b36b0cf86426c7c5286a1
김우성 <azunox9@gmail.com>
2026-04-14 12:05:04 +0900

    refactor : swagger 전역 인증 요구 제거

--- src/main/java/com/jinju/jinjuwiki/global/config/OpenApiConfig.java
+++ src/main/java/com/jinju/jinjuwiki/global/config/OpenApiConfig.java
@@ -3,7 +3,6 @@ package com.jinju.jinjuwiki.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +12,7 @@ public class OpenApiConfig {
 
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
+    // Swagger Bearer 인증 스키마 설정 클래스
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
@@ -27,7 +27,6 @@ public class OpenApiConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
-                        ))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+                        ));
     }
 }
