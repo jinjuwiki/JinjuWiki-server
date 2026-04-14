61114efac354b234d535995d98087e6d2cc39e35
김우성 <azunox9@gmail.com>
2026-04-14 14:55:38 +0900

    fix : category response swagger 스키마 추가

--- src/main/java/com/jinju/jinjuwiki/domain/category/dto/response/CategoryResponse.java
+++ src/main/java/com/jinju/jinjuwiki/domain/category/dto/response/CategoryResponse.java
@@ -1,7 +1,12 @@
 package com.jinju.jinjuwiki.domain.category.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "카테고리 응답")
 public record CategoryResponse(
+        @Schema(description = "카테고리 ID", example = "1")
         Long categoryId,
+        @Schema(description = "카테고리 이름", example = "역사")
         String name
 ) {
 }
